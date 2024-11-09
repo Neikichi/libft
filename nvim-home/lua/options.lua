75a0ddac4904a8f410a6cab1dabe6775015d2718
Vee92 <v.nkz92@gmail.com>
2024-11-09 21:12:40 +0800

    done done done

---
+++ nvim-home/lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.opt.expandtab = false    -- Use tabs instead of spaces
+vim.opt.shiftwidth = 4       -- Number of spaces used for each step of (auto)indent
+vim.opt.tabstop = 4          -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4      -- Number of spaces that a <Tab> counts for while editing
+vim.opt.ruler = true
+vim.opt.laststatus = 2
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
